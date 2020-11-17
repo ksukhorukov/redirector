8b9b7ed5379b54fb8f1f665a8142768bef357989
Kirill Sukhorukov <kirill.sukhorukov@akvelon.com>
2020-11-17 19:22:12 +0600

    short url generation testing

--- spec/services/redirector_service_spec.rb
+++ spec/services/redirector_service_spec.rb
@@ -4,7 +4,13 @@ RSpec.describe RedirectorService do
 	it "successfully generate salt for the uri" do 
 		service = RedirectorService.new({ url: "http://www.google.com" })
 		salt = service.send(:generate_salt)
-		expect(salt.length).to eq(10)
 		expect(salt).to match(/[a-z0-9]{10}/i)
+	end
+
+	it "successfully generate short url for the uri" do 
+		service = RedirectorService.new({ url: "http://www.google.com" })
+		result = service.create
+		short_url = service.create[:short_url]
+		expect(short_url).to match(/[a-z0-9]{5}/i)
 	end
 end
